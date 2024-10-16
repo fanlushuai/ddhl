00c0f6b03f738f9d7f186c084b79bbcf214ed4a5
auh <21078607+fanlushuai@users.noreply.github.com>
2024-10-16 17:56:45 +0800

    f

--- work.js
+++ work.js
@@ -15,6 +15,7 @@ function boot() {
     slog("切换到滴滴");
     bootV.boot2dd();
 
+    // todo 判断，当前位于哪个界面
     if (Config.ddxc) {
       slog("开启滴滴行程");
       bootV.intoDdxcView();
@@ -29,6 +30,8 @@ function boot() {
   if (Config.hlxc || Config.hldt) {
     slog("切换到哈啰");
     bootV.boot2hl();
+    // todo 判断，当前位于哪个界面,先开始哪个活动。
+    // 如果，几个目标界面，都没在。直接向后返回，然后重新进入
 
     if (Config.hlxc) {
       slog("开启哈啰 行程");
